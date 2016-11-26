76450a2a6ddd1f5d4bc9f20fd54d82453e0e515f
Reggie Mack <thegraphixninja@gmail.com>
2016-11-26 13:15:24 -0600

    build html with JQuery
    build html with JQuery

--- js/flickr.js
+++ js/flickr.js
@@ -11,7 +11,14 @@ $(document).ready(function() {
             format: "json"
         };
         function displayPhotos(data) {
-            
+            var photoHTML = '<ul>';
+            $.each( data.items, function (i, photo) {
+                photoHTML += '<li class="grid-25 tablet-grid-50">';
+                photoHTML += '<a href="' + photo.link + '" class="image">';
+                photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+            });
+            photoHTML += '</ul>';
+            $('#photos').html(photoHTML);
         }
         $.getJSON(flickerAPI, flickrOptions, displayPhotos);
     });
